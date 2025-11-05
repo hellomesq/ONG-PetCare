da23d8023783e0681658891ea30b417a1babab55
hellomesq <helo.alvz@gmail.com>
2025-11-05 00:19:38 -0300

    Criação do projeto

---
+++ src/main/java/com/petcare/ong/PetcareOngApplication.java
@@ -0,0 +1,13 @@
+package com.petcare.ong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetcareOngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetcareOngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petcare/ong/PetcareOngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petcare.ong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetcareOngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
